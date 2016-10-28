941ad4564dece54623252611cfbf2b11e2953b8f
Petr Besir Horacek <petr.horacek@legerete.cz>
2016-10-28 02:30:19 +0200

    SSH deploy fix

--- start.sh
+++ start.sh
@@ -7,11 +7,21 @@
 # Created Time: 2016/10/27
 #########################################################################
 
+if ($SSH_KEY_CHECKING_OFF); then
+	ssh -o StrictHostKeyChecking=no ${SSH_KEY_CHECKING_OFF}
+fi
+
+ssh -o StrictHostKeyChecking=no git@gitlab.legerete.cz
+
 cp /var/ssh-deploy-keys/* /etc/git-auto-deploy/.ssh/
+chmod 755 /etc/git-auto-deploy/.ssh
 chmod 600 /etc/git-auto-deploy/.ssh/*
 chown -R git-auto-deploy:git-auto-deploy /etc/git-auto-deploy
 
-service git-auto-deploy start --port 8080 --daemon-mode false
+ssh-keyscan -t rsa gitlab.legerete.cz  >> /etc/git-auto-deploy/.ssh/known_hosts
+chmod 644 /etc/git-auto-deploy/.ssh/known_hosts
+
+service git-auto-deploy start --port 8080
 service git-auto-deploy status
 
 # keep alive the container
