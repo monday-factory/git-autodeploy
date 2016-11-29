6eb7541f49aecf2decec8bbfa3b60615ac0edb6c
Petr Besir Horacek <petr.horacek@legerete.cz>
2016-11-30 00:21:00 +0100

    Fixed duplicate add record to known_hosts

--- start.sh
+++ start.sh
@@ -7,20 +7,17 @@
 # Created Time: 2016/10/27
 #########################################################################
 
-if ($SSH_KEY_CHECKING_OFF); then
-	echo "StrictHostKeyChecking no" > /etc/git-auto-deploy/.ssh/config
-fi
-
-ssh -o StrictHostKeyChecking=no git@$SSH_KEY_CHECKING_OFF
-
 cp /var/ssh-deploy-keys/* /etc/git-auto-deploy/.ssh/
 chmod 755 /etc/git-auto-deploy/.ssh
 chmod 600 /etc/git-auto-deploy/.ssh/*
 chown -R git-auto-deploy:git-auto-deploy /etc/git-auto-deploy
 
-echo "" > /etc/git-auto-deploy/.ssh/known_hosts
-ssh-keyscan -t rsa $SSH_KEY_CHECKING_OFF  >> /etc/git-auto-deploy/.ssh/known_hosts
-chmod 644 /etc/git-auto-deploy/.ssh/known_hosts
+if [ -n ${SSH_KEY_CHECKING_OFF:+x} ]; then
+	echo "StrictHostKeyChecking no" > /etc/git-auto-deploy/.ssh/config
+	ssh -o StrictHostKeyChecking=no git@$SSH_KEY_CHECKING_OFF
+	ssh-keyscan -t rsa $SSH_KEY_CHECKING_OFF  > /etc/git-auto-deploy/.ssh/known_hosts
+	chmod 644 /etc/git-auto-deploy/.ssh/known_hosts
+fi
 
 service git-auto-deploy start --port 8080
 service git-auto-deploy status
